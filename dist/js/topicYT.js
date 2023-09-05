c6e20aa2528cb642b46dfaceee0437764ae8ce2a
0nejean <onejean114@gmail.com>
2023-09-05 17:44:43 +0900

    d8 index > mission adjustment

---
+++ dist/js/topicYT.js
@@ -0,0 +1,19 @@
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onYouTubePlayerAPIReady() {
+  new YT.Player("topic_player", {
+    videoId: "EA46nq6klPw",
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: "EA46nq6klPw",
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute();
+      },
+    },
+  });
+}
--- readme.md
+++ readme.md
@@ -2,10 +2,10 @@
 
 > html/css/js > 웹표준/웹접근성 > 그누보드 + 폼 추가 > 반응형(1440-pc, 1024-tables, 428-mobiles)
 
-1. index - 웹표준/웹접근성 동영상/슬라이드 제외 v
+1. index - v
 2. about - v
-3. mission - text 추가 및 웹표준/웹접근성 작업
-4. pics n tv - 웹표준/웹접근성 작업
+3. mission - v
+4. pics n tv - v
 5. topic - 웹표준/웹접근성 작업
 6. play - not yet
 
@@ -17,4 +17,10 @@ https://css-validator.kldp.org/
 
 ## TO DO
 
-- TUE | mission/pics n tv / topic 웹표준 + 웹접근성 마무리
+- TUE |
+  v 토픽 상단의 수정
+  v 메인 어바웃 추가
+  v 미션 수정(상단 > 레이아웃 > 웹표준 + 접근)
+
+- WED :
+  > 토픽 수정(상단 > 레이아웃 > 웹표준 + 접근)
